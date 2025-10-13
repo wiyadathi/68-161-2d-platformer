d3bb55f4d50f933aa8bc7e6557aa8a9bc40e2228
wiyadathi <wiyada.t@bu.ac.th>
2025-10-13 12:32:33 +0700

    finish Character script

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public abstract class Character : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //attributes
+   private int health;
+   public int Health 
+   { 
+     get => health;  
+     set => health = (value < 0) ? 0 : value; 
+   }
+
+    protected Animator anim;
+    protected Rigidbody2D rb;
+    
+    
+    //Behavior
+    public void TakeDamage(int damage)
     {
-        
+        Health -= damage;
+        Debug.Log($"{this.name} tool {damage} damage! Current Health: {Health}");
+
+        IsDead();
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool IsDead() 
     {
-        
+        if (Health <= 0)
+        {
+            Destroy(this.gameObject);
+            Debug.Log(this.name + " is dead!");
+            return true;
+        }
+        else return false;
     }
+
 }
